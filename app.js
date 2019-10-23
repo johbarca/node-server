6e5914688f94fe89610ac37c7af616e7659e5fdd
barca <330492740@qq.com>
2019-10-23 17:00:35 +0800

    Merge branch 'master' of github.com:johbarca/node

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = new Koa();
 const router = require('./middleware/router');
 const koaBody = require('koa-body'); //解析上传文件的插件
 const cors = require('koa-cors'); //使用koa-cors解决跨域请求问题
+const staticFiles = require('koa-static');
+const session=require('koa-session');
 
 app.use(cors({
     origin: function (ctx) {
@@ -18,7 +20,16 @@ app.use(cors({
     allowMethods: ['GET', 'POST', 'DELETE'], //设置允许的HTTP请求类型
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }));
-//使用koa-bodyparser处理body读取问题
+app.keys = ['this is my secret key'];
+app.use(session({
+    key: 'koa:sess', /** cookie的名称，可以不管 */
+    maxAge: 1800000, /** (number) maxAge in ms (default is 1 days)，cookie的过期时间，这里表示半个小时 */
+    overwrite: true, /** (boolean) can overwrite or not (default true) */
+    httpOnly: true, /** (boolean) httpOnly or not (default true) */
+    signed: true, /** (boolean) signed or not (default true) */
+  },app));
+
+app.use(staticFiles(__dirname+"/public"))
 app.use(koaBody());
 app.use(router.routes());
 
--- middleware/sqlMap.js
+++ middleware/sqlMap.js
@@ -1,8 +1,12 @@
 var sqlMap = {
-    user:{
-        add: 'insert into user( name, password) values ( ?, ?)',
-        select_name: 'SELECT * from user where name = ?',    //查询 username
-        select_password: 'SELECT * from user where password = ?'      //查询 password
+    user: {
+        add: 'insert into user( id, name, password) values ( 0, ?, ?)',
+        select_name: 'SELECT * from user where name = ?', //查询 username
+        select_password: 'SELECT * from user where password = ?' //查询 password
+    },
+    file: {
+        add: 'insert into file( id, userName, name, path) values ( 0, ?, ?, ?)',
+        getFile: 'select name,path from file where userName = ?'
     }
 }
 
